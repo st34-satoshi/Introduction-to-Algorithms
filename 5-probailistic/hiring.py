ef885049fad1c657fd206c05a34f4d88336d2729
satoshi-tanaka <st34.satoshi@gmail.com>
2020-05-13 14:59:32 +0900

    add. able to do experiment of hiring average cost

--- 5-probailistic/hiring.py
+++ 5-probailistic/hiring.py
@@ -24,12 +24,30 @@ def hire_assistant_test():
     print(f"max ability in candidates = {max(candidates)}")
 
 
-def hire_mean_count():
-    # TODO:
+def average_number_hiring(candidates):
+    ans = 0
+    for i in range(1, len(candidates)+1):
+        ans += 1 / i
+    return ans
+
+
+def hire_experiment():
     # mean count of hiring
-    E = 100  # number of experiments
+    E = 1000  # number of experiments
     N = 30  # size of candidates
+    total_hiring_cnt = 0
+    for _ in range(E):
+        # prepare data
+        candidates = [random.randint(1, N**3) for _ in range(N)]
+        count, _ = hire_assistant(candidates)
+        total_hiring_cnt += count
+    print(f"number of experiments = {E}")
+    print(f"size of candidates = {N}")
+    print(f"mean of the number of hiring = {total_hiring_cnt/E}")
+    average_hiring = average_number_hiring(candidates)
+    print(f"computed average hiring number = {average_hiring}")
 
 
 if __name__ == '__main__':
-    hire_assistant_test()
+    # hire_assistant_test()
+    hire_experiment()
