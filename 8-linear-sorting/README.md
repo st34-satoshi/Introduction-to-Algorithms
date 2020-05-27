d3dca541ae21bc1d77f6024d7b0a434a3f89cbae
satoshi-tanaka <st34.satoshi@gmail.com>
2020-05-27 12:42:07 +0900

    add. in 8, counting sort

---
+++ 8-linear-sorting/README.md
@@ -0,0 +1,4 @@
+## Sorting in linear time
+
+## 8.2 counting sort
+`python counting_sort.py`
---
+++ 8-linear-sorting/counting_sort.py
@@ -0,0 +1,31 @@
+import random
+
+
+def gen_numbers(size, max_number):
+    numbers = [random.randint(0, max_number) for _ in range(size)]
+    return numbers
+
+
+def counting_sort(numbers, max_number):
+    c = [0]*(max_number+1)
+    # count the number
+    for n in numbers:
+        c[n] += 1
+    # count the cumulative sum
+    for i in range(1, len(c)):
+        c[i] += c[i-1]
+    print(c)
+    ans = [0]*len(numbers)
+    for i in range(len(numbers)):
+        ans[c[numbers[i]]-1] = numbers[i]
+        c[numbers[i]] -= 1
+    return ans
+
+
+if __name__ == '__main__':
+    MAX = 10
+    SIZE = 10
+    a = gen_numbers(SIZE, MAX)
+    print(a)
+    sorted_a = counting_sort(a, MAX)
+    print(sorted_a)
