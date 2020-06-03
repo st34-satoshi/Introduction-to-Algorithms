ca744ba6dd7088451f3fdbbb5b96e6727517fcb7
satoshi-tanaka <st34.satoshi@gmail.com>
2020-06-03 16:27:03 +0900

    add. quick sort in 7

---
+++ 7-quick-sort/README.md
@@ -0,0 +1,4 @@
+## 7 quick sort
+
+## quick sort
+`python quick_sort.py` 
---
+++ 7-quick-sort/quick_sort.py
@@ -0,0 +1,30 @@
+import random
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] < x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i+1], A[r] = A[r], A[i+1]
+    return i + 1
+
+
+def quick_sort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quick_sort(A, p, q-1)
+        quick_sort(A, q+1, r)
+
+
+def test():
+    size = 10
+    numbers = [random.randint(0, 10) for _ in range(size)]
+    print(numbers)
+    quick_sort(numbers, 0, size-1)
+    print(numbers)
+
+
+if __name__ == '__main__':
+    test()
